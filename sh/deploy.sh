5af5b5bb4baa64b8c39c2a47fb27bc0f72ef0c99
heidi-luong1109 <heidi.luong1109@yahoo.com>
2021-07-05 18:59:58 +1000

    update

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -14,11 +14,11 @@
 sys_time=`date "+%Y%m%d_%H%M%S"`
 #Refresh git folder
 echo "----1.git processing"
-#Let's pretend your project is in /home/git/YourProjectName
+#Let's pretend your project is in /home/git/game
 if [ -d /home/git ]
 then
     echo "Git folder exists."
-    cd /home/git/YourProjectName
+    cd /home/git/game
     git pull
 else
     mkdir /home/git
@@ -27,41 +27,41 @@ else
     git clone https://github.com/heidi-luong1109/game
 fi
 
-#Deploy YourProjectName
+#Deploy game
 echo "----2.backing up PHP"
-cd /var/www/html/
-if [ -d /var/www/html/YourProjectName ]
+cd /home/slot.heidi.net.au/public_html/
+if [ -d /home/slot.heidi.net.au/public_html/game ]
 then
-    echo "YourProjectName exists."
+    echo "game exists."
     if [ -d /home/back ]
     then
         echo "Backup folder exists."
     else
         mkdir /home/back
     fi
-    mv /var/www/html/YourProjectName /home/back/YourProjectName_${sys_time}
-    mkdir /var/www/html/YourProjectName
+    mv /home/slot.heidi.net.au/public_html/game /home/back/game_${sys_time}
+    mkdir /home/slot.heidi.net.au/public_html/game
 else
     #some config file creation for the first time deployment.一些首次部署的config设置
     #...
-    echo "YourProjectName config files are created."
+    echo "game config files are created."
 fi
 echo "----3.deploying PHP."
 cd /var/www/html
-cp -r /home/git/YourProjectName ./YourProjectName
+cp -r /home/git/game ./game
 #config change.你的项目部署需要的config修改
 #...
 
 #MySQL backup
 echo "----5.backing up DB..."
 backupFile=/tmp/DB_backup${sys_time}.sql
-mysqldump -uroot -p${pwd} YourProjectName>${backupFile}
+mysqldump -uroot -p${pwd} game>${backupFile}
 
 #MySQL script deployment
 echo "----6.deploying SQLs"
-if [ -f /home/git/YourProjectName/sql/deployment/*.sql ]
+if [ -f /home/git/game/sql/deployment/*.sql ]
 then
-    for FILE in /home/git/YourProjectName/sql/deployment/*.sql
+    for FILE in /home/git/game/sql/deployment/*.sql
     do
         sys_time=`date "+%Y%m%d_%H%M%S"`
         mysql -uroot -p${pwd} -e "source $FILE" | tee /tmp/DB_log_${sys_time}.sql
